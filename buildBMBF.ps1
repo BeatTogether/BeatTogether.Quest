52027eb57a35de4b597b33c7bb682d3887d0fb7a
Robert Kopaczewski <rk@23doors.com>
2020-11-15 19:34:12 +0100

    makefile for mac/linux/wsl

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -1,5 +1,6 @@
 # Builds a .zip file for loading with BMBF
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+$VERSION = Get-Content $PSScriptRoot/VERSION
 
 $buildScript = "$NDKPath/build/ndk-build"
 if (-not ($PSVersionTable.PSEdition -eq "Core")) {
@@ -7,4 +8,4 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libbeattogether.so", "./libs/arm64-v8a/libbeatsaber-hook_0_8_4.so", "./libs/arm64-v8a/libcodegen_0_4_0.so", "./Cover.png", "./bmbfmod.json" -DestinationPath "./BeatTogether_v0.3.0.zip" -Update
+Compress-Archive -Path "./libs/arm64-v8a/libbeattogether.so", "./libs/arm64-v8a/libbeatsaber-hook_0_8_4.so", "./libs/arm64-v8a/libcodegen_0_4_0.so", "./Cover.png", "./bmbfmod.json" -DestinationPath "./BeatTogether_$VERSION.zip" -Update
