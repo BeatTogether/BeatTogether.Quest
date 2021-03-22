3b8a86e9660c88fd95665f24a549b2bf93e64f96
Lauriethefish <lauriethefish@outlook.com>
2021-03-22 19:04:18 +0000

    Update dependencies

--- Android.mk
+++ Android.mk
@@ -8,18 +8,18 @@ TARGET_ARCH_ABI := arm64-v8a
 include $(CLEAR_VARS)
 LOCAL_MODULE := hook
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
-# Creating prebuilt for dependency: codegen - version: 0.6.2
+# Creating prebuilt for dependency: codegen - version: 0.7.0
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_6_2
+LOCAL_MODULE := codegen_0_7_0
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_6_2.so
+LOCAL_SRC_FILES := extern/libcodegen_0_7_0.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.0.12
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.2.3
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_0_12
+LOCAL_MODULE := beatsaber-hook_1_2_3
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_12.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_2_3.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: modloader - version: 1.0.4
 include $(CLEAR_VARS)
@@ -34,8 +34,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_12
-LOCAL_SHARED_LIBRARIES += codegen_0_6_2
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_3
+LOCAL_SHARED_LIBRARIES += codegen_0_7_0
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"BeatTogether"' -DVERSION='"$(VERSION)"' -DHOST_NAME='"$(HOST_NAME)"' -DPORT='$(PORT)' -DSTATUS_URL='$(STATUS_URL)' -I'./shared' -I'./extern' -isystem'./extern/codegen/include'
 LOCAL_CPPFLAGS += -std=c++2a
