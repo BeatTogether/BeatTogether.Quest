7b6cb70ebef20504e023da909cfc6be5358b97f6
Lauriethefish <lauriethefish@outlook.com>
2021-03-24 17:37:42 +0000

    Fix file version is incorrect

--- buildQP.ps1
+++ buildQP.ps1
@@ -6,9 +6,9 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "BeatTogether_v0.1.2.qmod"
-$TempArchiveName = "beattogether_v0.1.2.qmod.zip"
+$ArchiveName = "BeatTogether_v1.1.0.qmod"
+$TempArchiveName = "beattogether_v1.1.0.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libbeattogether.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libbeattogether.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./mod.json" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
