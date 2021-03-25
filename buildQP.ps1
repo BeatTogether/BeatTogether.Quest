6824db9c4a02ae4a749afab3349ffde407d6f5ed
Lauriethefish <lauriethefish@outlook.com>
2021-03-25 20:40:32 +0000

    Version bump 1.2.0

--- buildQP.ps1
+++ buildQP.ps1
@@ -6,8 +6,8 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "BeatTogether_v1.1.0.qmod"
-$TempArchiveName = "beattogether_v1.1.0.qmod.zip"
+$ArchiveName = "BeatTogether_v1.2.0.qmod"
+$TempArchiveName = "beattogether_v1.2.0.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 Compress-Archive -Path "./libs/arm64-v8a/libbeattogether.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./mod.json" -DestinationPath $TempArchiveName -Force
