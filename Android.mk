af348227eeab913d2151460b87acd8f2c5c4d165
Lauriethefish <lauriethefish@outlook.com>
2021-04-13 20:56:03 +0100

    Bump dependency versions

--- Android.mk
+++ Android.mk
@@ -8,18 +8,18 @@ TARGET_ARCH_ABI := arm64-v8a
 include $(CLEAR_VARS)
 LOCAL_MODULE := hook
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
-# Creating prebuilt for dependency: codegen - version: 0.7.1
+# Creating prebuilt for dependency: codegen - version: 0.7.2
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_7_1
+LOCAL_MODULE := codegen_0_7_2
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_7_1.so
+LOCAL_SRC_FILES := extern/libcodegen_0_7_2.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.0
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.3
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_3_0
+LOCAL_MODULE := beatsaber-hook_1_3_3
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_0.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_3.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: modloader - version: 1.1.0
 include $(CLEAR_VARS)
@@ -34,8 +34,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_0
-LOCAL_SHARED_LIBRARIES += codegen_0_7_1
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_3
+LOCAL_SHARED_LIBRARIES += codegen_0_7_2
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"BeatTogether"' -DVERSION='"$(VERSION)"' -DHOST_NAME='"$(HOST_NAME)"' -DPORT='$(PORT)' -DSTATUS_URL='$(STATUS_URL)' -I'./shared' -I'./extern' -isystem'./extern/codegen/include'
 LOCAL_CPPFLAGS += -std=c++2a
--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -10,5 +10,5 @@ $ArchiveName = "BeatTogether_v1.2.0.qmod"
 $TempArchiveName = "beattogether_v1.2.0.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libbeattogether.so", "./libs/arm64-v8a/libbeatsaber-hook_1_3_0.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libbeattogether.so", "./libs/arm64-v8a/libbeatsaber-hook_1_3_3.so", "./mod.json" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
