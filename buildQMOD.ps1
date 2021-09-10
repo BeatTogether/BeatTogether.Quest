10c3b19d437da68bf42d938e4a30987213168ddc
EnderdracheLP <enderdrachelp@gmail.com>
2021-09-10 20:33:57 +0200

    Update to 1.17.1, removed unnecessary stuff

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -11,6 +11,6 @@ $ArchiveName = "BeatTogether_v$VERSION.qmod"
 $TempArchiveName = "beattogether_v$VERSION.qmod.zip"
 $BS_HOOK_VERSION = "2_0_3"
 
-& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk VERSION=$VERSION
 Compress-Archive -Path "./libs/arm64-v8a/libbeattogether.so", "./libs/arm64-v8a/libbeatsaber-hook_$BS_HOOK_VERSION.so", "./mod.json", "./Cover.png" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
