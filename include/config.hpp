f58ace6b2efb1900b73ef139bd0c3e764f54b92f
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-10 17:24:12 +0200

    Default to 10 max players

--- include/config.hpp
+++ include/config.hpp
@@ -10,9 +10,13 @@
 #error "Define STATUS_URL!"
 #endif
 
+#ifndef MAX_PLAYER_COUNT
+#define MAX_PLAYER_COUNT 10
+#endif
+
 struct Config {
     MultiplayerCore::ServerConfig serverConfig = MultiplayerCore::ServerConfig(
-        GRAPH_URL, STATUS_URL
+        GRAPH_URL, STATUS_URL, MAX_PLAYER_COUNT
     );
 
     std::string button = "Modded\nOnline";
