5d05ad138467be6873cba7c4675e6fdcb71c8966
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-19 15:57:12 +0200

    Add catch wrapper

--- include/hooking.hpp
+++ include/hooking.hpp
@@ -51,13 +51,13 @@ public:
         static const MethodInfo* getInfo() { return ::il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::get(); }                               \
         static funcType* trampoline() { return &name_; }                                                                                            \
         static inline retval (*name_)(__VA_ARGS__) = nullptr;                                                                                       \
-        static funcType hook() { return hook_##name_; }                                                                                             \
+        static funcType hook() { return &::Hooking::HookCatchWrapper<&hook_##name_, funcType>::wrapper; }                                           \
         static retval hook_##name_(__VA_ARGS__);                                                                                                    \
     };                                                                                                                                              \
     AUTO_INSTALL(name_)                                                                                                                             \
     retval Hook_##name_::hook_##name_(__VA_ARGS__)
 
-#define MAKE_AUTO_HOOK_FIND_VERBOSE(name_, infoGet, retval, ...)                                                 \
+#define MAKE_AUTO_HOOK_FIND_VERBOSE(name_, infoGet, retval, ...)                                            \
     struct Hook_##name_ {                                                                                   \
         constexpr static const char* name() { return #name_; }                                              \
         static const MethodInfo* getInfo() { return infoGet; }                                              \
@@ -67,7 +67,7 @@ public:
         static funcType hook() { return &::Hooking::HookCatchWrapper<&hook_##name_, funcType>::wrapper; }   \
         static retval hook_##name_(__VA_ARGS__);                                                            \
     };                                                                                                      \
-    AUTO_INSTALL_ORIG(name_)                                                                                \
+    AUTO_INSTALL(name_)                                                                                     \
     retval Hook_##name_::hook_##name_(__VA_ARGS__)
 
 #define MAKE_AUTO_HOOK_ORIG_MATCH(name_, mPtr, retval, ...)                                                                                         \
@@ -79,7 +79,7 @@ public:
         static const MethodInfo* getInfo() { return ::il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::get(); }                               \
         static funcType* trampoline() { return &name_; }                                                                                            \
         static inline retval (*name_)(__VA_ARGS__) = nullptr;                                                                                       \
-        static funcType hook() { return hook_##name_; }                                                                                             \
+        static funcType hook() { return &::Hooking::HookCatchWrapper<&hook_##name_, funcType>::wrapper; }                                           \
         static retval hook_##name_(__VA_ARGS__);                                                                                                    \
     };                                                                                                                                              \
     AUTO_INSTALL_ORIG(name_)                                                                                                                        \
