7ff15dd85251a63ebd8c1386e6b322957141bfd6
EnderdracheLP <enderdrachelp@elp.quest>
2024-07-10 20:30:35 +0200

    Auth hook, try getting token if that failes return custom token

--- src/Hooks/PlatformAuthenticationTokenProvider.cpp
+++ src/Hooks/PlatformAuthenticationTokenProvider.cpp
@@ -1,5 +1,6 @@
 #include "hooking.hpp"
 #include "logging.hpp"
+#include "tasks.hpp"
 
 #include "System/Threading/Tasks/Task_1.hpp"
 #include "GlobalNamespace/AuthenticationToken.hpp"
@@ -8,16 +9,22 @@
 static ConstString dummy_auth("who_is_rem_?");
 
 MAKE_AUTO_HOOK_ORIG_MATCH(PlatformAuthenticationTokenProvider_GetAuthenticationToken, &GlobalNamespace::PlatformAuthenticationTokenProvider::GetAuthenticationToken, System::Threading::Tasks::Task_1<GlobalNamespace::AuthenticationToken>*, GlobalNamespace::PlatformAuthenticationTokenProvider* self) {
-    try {
-        return task = PlatformAuthenticationTokenProvider_GetAuthenticationToken(self);
-    } catch (const std::exception& e) {
-        ERROR("Failed to get authentication token: {}", e.what());
-        DEBUG("Returning custom authentication token!");
-        return System::Threading::Tasks::Task_1<GlobalNamespace::AuthenticationToken>::New_ctor(GlobalNamespace::AuthenticationToken(
+    auto t = PlatformAuthenticationTokenProvider_GetAuthenticationToken(self);
+    return BeatTogether::StartTask<GlobalNamespace::AuthenticationToken>([=](){
+        using namespace std::chrono_literals;
+        while (!(t->IsCompleted || t->IsCanceled)) std::this_thread::sleep_for(50ms);
+        GlobalNamespace::AuthenticationToken token = t->ResultOnSuccess;
+        if (token) {
+            INFO("Successfully got auth token, returning it!");
+            token.platform = GlobalNamespace::AuthenticationToken::Platform::OculusQuest; // Makes sure platform is set to OculusQuest
+            return token;
+        }
+        ERROR("Failed to get auth token, returning custom authentication token!");
+        return GlobalNamespace::AuthenticationToken(
             GlobalNamespace::AuthenticationToken::Platform::OculusQuest,
             self->_userId, // Important for server and client to keep track of things, should not be modified
             self->_userName,
             dummy_auth
-        ));
-    }
+        );
+    });
 }
