4005a3bf2e0acc4635996d07fd71a9377052af86
EnderdracheLP <enderdrachelp@elp.quest>
2024-08-30 21:05:46 +0200

    Fixed logging

--- include/logging.hpp
+++ include/logging.hpp
@@ -13,8 +13,8 @@ template <> struct fmt::formatter<::StringW> : formatter<string_view> {
     }
 };
 
-#define INFO(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::INF>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
-#define ERROR(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::ERR>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
-#define CRITICAL(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::ERR>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
-#define DEBUG(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::DBG>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
-#define WARNING(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::WRN>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
+#define INFO(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::INF>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
+#define ERROR(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
+#define CRITICAL(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
+#define DEBUG(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::DBG>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
+#define WARNING(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::WRN>(str, "BeatTogether" __VA_OPT__(, __VA_ARGS__))
